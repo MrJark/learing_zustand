fc4926e2d486a6fb1392a54fdfc388fbca57382e
Chema <mrjark7@gmail.com>
2023-11-10 19:35:32 +0100

    created all definitions bears and components

--- src/pages/01-basic/BearPage.tsx
+++ src/pages/01-basic/BearPage.tsx
@@ -1,6 +1,9 @@
 import { WhiteCard } from '../../components';
+import { useBearStore } from '../../stores';
 
 export const BearPage = () => {
+  // const blackBear = useBearStore( state => state.blackBears)
+  // const increaseBlackBears = useBearStore( state => state.increaseBlackBears)
   return (
     <>
       <h1>Contador de Osos</h1>
@@ -9,18 +12,18 @@ export const BearPage = () => {
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
 
-        <WhiteCard centered>
+        {/* <WhiteCard centered>
           <h2>Osos Negros</h2>
 
           <div className="flex flex-col md:flex-row">
-            <button> +1</button>
-            <span className="text-3xl mx-2 lg:mx-10"> 0 </span>
-            <button>-1</button>
+            <button onClick={() => increaseBlackBears( +1 )}> +1</button>
+            <span className="text-3xl mx-2 lg:mx-10"> {blackBear} </span>
+            <button onClick={ () => increaseBlackBears( -1 ) }>-1</button>
           </div>
 
-        </WhiteCard>
+        </WhiteCard> */}
 
-        <WhiteCard centered>
+        {/* <WhiteCard centered>
           <h2>Osos Polares</h2>
 
           <div className="flex flex-col md:flex-row">
@@ -41,9 +44,10 @@ export const BearPage = () => {
             <button>-1</button>
           </div>
 
-        </WhiteCard>
-
-
+        </WhiteCard> */}
+        <BlackBear />
+        <PandaBear />
+        <PolarkBear />
 
 
       </div>
@@ -51,3 +55,56 @@ export const BearPage = () => {
     </>
   );
 };
+
+
+
+export const BlackBear = () => {
+  const blackBear = useBearStore( state => state.blackBears )
+  const increaseBlackBears = useBearStore( state => state.increaseBlackBears )
+  return (
+    <WhiteCard centered>
+      <h2>Osos Negros</h2>
+
+      <div className="flex flex-col md:flex-row">
+        <button onClick={ () => increaseBlackBears( +1 ) }> +1</button>
+        <span className="text-3xl mx-2 lg:mx-10"> { blackBear } </span>
+        <button onClick={ () => increaseBlackBears( -1 ) }>-1</button>
+      </div>
+
+    </WhiteCard>
+  )
+}
+
+export const PolarkBear = () => {
+  const polarBear = useBearStore( state => state.polarBears )
+  const increasePolarBears = useBearStore( state => state.increasePolarBears )
+  return (
+    <WhiteCard centered>
+      <h2>Osos Negros</h2>
+
+      <div className="flex flex-col md:flex-row">
+        <button onClick={ () => increasePolarBears( +1 ) }> +1</button>
+        <span className="text-3xl mx-2 lg:mx-10"> { polarBear } </span>
+        <button onClick={ () => increasePolarBears( -1 ) }>-1</button>
+      </div>
+
+    </WhiteCard>
+  )
+}
+
+export const PandaBear = () => {
+  const pandaBear = useBearStore( state => state.pandaBears )
+  const increasePandaBear = useBearStore( state => state.increasePandaBears )
+  return (
+    <WhiteCard centered>
+      <h2>Osos Negros</h2>
+
+      <div className="flex flex-col md:flex-row">
+        <button onClick={ () => increasePandaBear( +1 ) }> +1</button>
+        <span className="text-3xl mx-2 lg:mx-10"> { pandaBear } </span>
+        <button onClick={ () => increasePandaBear( -1 ) }>-1</button>
+      </div>
+
+    </WhiteCard>
+  )
+}
--- src/stores/bears/bears.store.ts
+++ src/stores/bears/bears.store.ts
@@ -8,13 +8,17 @@ interface BearState {
   pandaBears: number,
 
   increaseBlackBears: (by: number) => void
+  increasePolarBears: (by: number) => void
+  increasePandaBears: (by: number) => void
 }
 
 export const useBearStore = create<BearState>()( ( set ) => ( {
-  blackBears: 0,
+  blackBears: 1,
   pandaBears: 1,
   polarBears: 10,
 
   increaseBlackBears: (by: number) => set((state) => ({blackBears: state.blackBears + by})),
+  increasePolarBears: (by: number) => set((state) => ({polarBears: state.polarBears + by })),
+  increasePandaBears: (by: number) => set((state) => ({pandaBears: state.pandaBears + by })),
 
 } ) )
